9df217e6d29eb4c48a43251ebce76a19679fca84
maxwellium <max@lawyer.com>
2024-03-15 20:20:21 +0000

    feat: return meaningful assertion errors for jwt validation

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,61 @@
-import { createHmac } from 'crypto';
+import { createHmac } from 'node:crypto';
 
-export const HEADER_HS256 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+export const HEADER_HS256_DEFAULT = { alg: 'HS256', typ: 'JWT' };
 
-export function generateHS256Token<T>(payload: T, secret: string) {
-  const payloadB64url = Buffer
-    .from(JSON.stringify(payload), 'utf-8')
-    .toString('base64url');
-  const data = `${ HEADER_HS256 }.${ payloadB64url }`;
+export const b64urlToJson = <T = Record<string, unknown>>(b64url: string) => JSON.parse(
+  Buffer.from(b64url, 'base64url').toString('utf8')
+) as T;
 
-  return `${ data }.${ signHS256(data, secret).toString('base64url') }`;
+export const jsonToB64url = <T = Record<string | number | symbol, unknown>>(payload: T) => Buffer
+  .from(JSON.stringify(payload), 'utf-8')
+  .toString('base64url');
+
+
+export function generateHS256Token<T = Record<string, unknown>>(
+  payload: T,
+  secret: string,
+  header: Record<string | number | symbol, unknown> = HEADER_HS256_DEFAULT
+) {
+  const data = `${jsonToB64url(header)}.${jsonToB64url(payload)}`;
+  return `${data}.${signHS256(data, secret)}`;
 }
 
 
 export function signHS256(data: string, secret: string) {
   return createHmac('sha256', secret, { encoding: 'utf-8' })
     .update(Buffer.from(data, 'utf-8'))
-    .digest();
+    .digest()
+    .toString('base64url');
 }
 
-export function verifyHS256Token(token: string, secret: string) {
-  const [ header, payload, signature ] = token.split('.');
+/** will throw if header corrupt or signature mismatch */
+export function assertHS256TokenSignature(token: string, secret: string) {
+  const [headerB64Url, payloadB64Url, signatureB64Url] = token.split('.');
 
-  if (HEADER_HS256 !== header) { return false; }
-
-  const verify = signHS256([ header, payload ].join('.'), secret).toString('base64url');
-
-  return verify === signature;
-}
-
-
-export function extractHS256Token<T>(token: string, secret: string) {
-
-  const verify = verifyHS256Token(token, secret);
-
-  if (!verify) {
-    throw new Error('signature mismatch');
+  let header: Record<string, unknown>;
+  try {
+    header = b64urlToJson(headerB64Url);
+  } catch (e) {
+    throw new Error('jose header corrupted');
   }
+  if ('object' !== typeof header || null === header) {
+    throw new Error('jose header corrupted');
+  }
+  if ('HS256' !== header.alg) {
+    throw new Error('jose header algorithm is not HS256');
+  }
+  if (signHS256([headerB64Url, payloadB64Url].join('.'), secret) !== signatureB64Url) {
+    throw new Error('HS256 signature mismatch');
+  }
+  return void 0;
+}
 
-  const [ , payloadB64Url ] = token.split('.');
 
-  const payload = Buffer.from(payloadB64Url, 'base64url').toString('utf8');
-
-  return JSON.parse(payload) as T;
+export function extractHS256Token<T = Record<string | number | symbol, unknown>>(
+  token: string,
+  secret: string
+) {
+  assertHS256TokenSignature(token, secret);
+  const [, payloadB64Url] = token.split('.');
+  return b64urlToJson<T>(payloadB64Url);
 }
